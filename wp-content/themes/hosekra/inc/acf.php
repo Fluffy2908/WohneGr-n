19f82d16d14579ea251a00063f95897ede08aa73
Alen <alen.sprem@gmail.com>
2026-01-04 18:46:53 +0100

    Popravek fatal error

--- wp-content/themes/hosekra/inc/acf.php
+++ wp-content/themes/hosekra/inc/acf.php
@@ -1310,18 +1310,6 @@ if (function_exists('acf_add_options_page')) {
 }
 
 /**
- * Varnostna funkcija za get_field z možnostjo privzete vrednosti
- */
-function alpenhomes_get_field($field_name, $post_id = false, $default = '')
-{
-    if (function_exists('get_field')) {
-        $value = get_field($field_name, $post_id);
-        return !empty($value) ? $value : $default;
-    }
-    return $default;
-}
-
-/**
  * Varnostna funkcija za globalna ACF polja (Options Page)
  */
 function alpenhomes_get_option($field_name, $default = '')
